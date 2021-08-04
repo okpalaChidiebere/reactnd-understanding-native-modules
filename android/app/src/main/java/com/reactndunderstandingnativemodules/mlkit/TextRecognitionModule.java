b19c5538b76084140d6fca2c56fc53711132919d
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-04 03:20:47 -0600

    Android Native Module - Add Text Recognition API in Java
    - We added the mlKit dependency
    - Used the API in our recognizeImage() method in Java side
    
    We pretty much followed the protocol here
    https://developers.google.com/ml-kit/vision/text-recognition/android#java
    
    The video here
    
    You can also have alook at the quickStart sample code to see the configurations

--- android/app/src/main/java/com/reactndunderstandingnativemodules/mlkit/TextRecognitionModule.java
+++ android/app/src/main/java/com/reactndunderstandingnativemodules/mlkit/TextRecognitionModule.java
@@ -1,5 +1,8 @@
 package com.reactndunderstandingnativemodules.mlkit;
 
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -7,6 +10,15 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognizerOptions;
+
+import java.io.IOException;
 
 public class TextRecognitionModule extends ReactContextBaseJavaModule {
     TextRecognitionModule(ReactApplicationContext context) {
@@ -20,6 +32,46 @@ public class TextRecognitionModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void recognizeImage(String url) {
+        Uri uri = Uri.parse(url); //url gotten from JS side
+        InputImage image;
+        try {
+            image = InputImage.fromFilePath(getReactApplicationContext(), uri);
+
+            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+
+            recognizer.process(image)
+                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                        @Override
+                        public void onSuccess(Text result) {
+                            String resultText = result.getText();
+                            for (Text.TextBlock block : result.getTextBlocks()) {
+                                String blockText = block.getText();
+                                Point[] blockCornerPoints = block.getCornerPoints();
+                                Rect blockFrame = block.getBoundingBox();
+                                for (Text.Line line : block.getLines()) {
+                                    String lineText = line.getText();
+                                    Point[] lineCornerPoints = line.getCornerPoints();
+                                    Rect lineFrame = line.getBoundingBox();
+                                    for (Text.Element element : line.getElements()) {
+                                        String elementText = element.getText();
+                                        Point[] elementCornerPoints = element.getCornerPoints();
+                                        Rect elementFrame = element.getBoundingBox();
+                                    }
+                                }
+                            }
+                        }
+                    })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Task failed with an exception
+                                    // ...
+                                }
+                            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Log.d("TextRecognitionModule", "Url: " + url); //to confirm that the file uri we passed from JS side is gotten here :)
     }
 }
