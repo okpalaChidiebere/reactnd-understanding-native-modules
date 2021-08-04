84443a9d94c3d6844392f0ecde6004cab8f0d6ca
okpalaChidiebere <okpalacollins4@gmail.com>
2021-08-04 06:33:03 -0600

    iOS Native Module - Add template
    https://reactnative.dev/docs/native-modules-ios

--- ios/MLKit/RCTTextRecognitionModule.h
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -5,8 +5,15 @@
 //  Created by Chidiebere Okpala Collins on 2021-08-04.
 //
 
-#ifndef RCTTextRecognitionModule_h
-#define RCTTextRecognitionModule_h
+#ifndef RCTTextRecognitionModule_h //this checks if the file is not included so that i can define it
+#define RCTTextRecognitionModule_h //check to see if the file is already defined
+
+//  RCTCalendarModule.h
+#import <React/RCTBridgeModule.h>
+//create our class interface
+@interface RCTTextRecognitionModule : NSObject <RCTBridgeModule>
+
+@end
 
 
 #endif /* RCTTextRecognitionModule_h */
